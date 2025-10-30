364638d18f8c5e931143461cdbe7d5edacd541db
irgiharnoyo <rasqasakim@gmail.com>
2025-10-30 07:49:14 +0700

    e-commerce lite sukses

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -60,7 +60,7 @@ const Dashboard: React.FC = () => {
       )}
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {products.map(product => (
+        {[...products].reverse().map(product => (
           <ProductCard 
             key={product.id} 
             product={product} 
